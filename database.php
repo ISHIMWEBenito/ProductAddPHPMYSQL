149bbe1a46ba56a9713880bf1e4e2eefba92bfdb
Benito Ishimwe <73600412+Benish-31@users.noreply.github.com>
2022-04-28 23:07:25 +0200

    Update database.php
    Real database connection

--- database.php
+++ database.php
@@ -3,14 +3,14 @@ if (!defined("DB_TYPE")) {
     define("DB_TYPE", "mysql");
 }
 if (!defined("DB_HOST")) {
-    define("DB_HOST", "localhost");
+    define("DB_HOST", "eu-cdbr-west-02.cleardb.net");
 }
 if (!defined("DB_NAME")) {
-    define("DB_NAME", "scandiweb3");
+    define("DB_NAME", "heroku_5b171f9582ac7db");
 }
 if (!defined("DB_PWD")) {
-    define("DB_PWD", "negro");
+    define("DB_PWD", "7512e5f6");
 }
 if (!defined("DB_USER")) {
-    define("DB_USER", "root");
+    define("DB_USER", "bc51c83e66a5de");
 }
